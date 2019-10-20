bcb4e58532b993c042f29723d9eb36065a3185e4
Aaron Ali <aaronali@email.com>
2019-10-20 16:12:17 +0100

    Changes to debian setup

--- SetupScripts/debian/setup.sh
+++ SetupScripts/debian/setup.sh
@@ -1,38 +1,124 @@
+\##################################################################
+#                            HyperledgerVMS                      #
+#                        HyperledgerVMs.appspot.com              #
+##################################################################
+# setup.sh                                                       #
+#     this scripts will auto install hyperledger fabrics various #
+#     components on linux debian                                 #
+##################################################################
+#  Created by		: Aaron Ali				 #
+#  Email		: aaronali@email.com			 #
+#  Created On		: 2019-10-19				 #
+#  Last update		: 2019-10-19				 #
+##################################################################
+
 #!/bin/bash
-cd /home/workstation
+cd /tmp
 
-sudo apt install -y curl
-sudo apt install -y docker
-sudo apt install -y docker-compose
-usermode -aG docker workstation
-sudo systemctl enable docker
-sudo systemctl start docker
-mkdir -p go/src/github.com/hyperledger
-mkdir -p go/src/github.com/workstation
-cd  $HOME/Downloads
-wget https://dl.google.com/go/go1.13.1.linux-amd64.tar.gz
+apt install -y curl
+apt install -y git
+apt install -y gcc
+apt install -y make
+apt install -y sudo
+apt install -y dnsutils
+apt install -y docker
+apt install -y docker-compose
+apt install -y ssh
+echo " **************************"
+echo " Starting services"
+echo " **************************"
+systemctl start ssh
+systemctl unmask docker.service
+systemctl unmask docker.socket
+systemctl unmask docker
+systemctl start docker.service
+systemctl start docker.socket
+systemctl start docker
+echo "Enable docker on start up ? [ Y | * / no]"
+read ans1 -p
+case $ans1 in
+ y | Y )
+  systemctl enable docker
+  ;;
+esac
+
+echo "Enable ssh on start uo [ Y | * / no ]"
+read ans2 -p
+case $ans2 in
+y | Y )
+  systemctl enable ssh
+  ;;
+esac
+
+wget "https://dl.google.com/go/go1.13.1.linux-amd64.tar.gz"
 tar -xvf go1.13.1.linux-amd64.tar.gz
-sudo mv go  /usr/local/go1.13.1
-sudo chown workstation /etc/profile
-sudo echo "export GOROOT=/usr/local/go1.13.1" >> /etc/profile
-sudo echo "export GOPATH=/home/workstation/go" >> /etc/profile
-source /etc/profile
-sudo echo "export PATH=${GOROOT}/bin:${GOPATH/bin}:${PATH}" >> /etc/profile
-sudo echo "export WORKSTATION=/home/workstation/go/src/github.com/workstation" >> /etc/profile
-sudo echo "export HYPERLEDGER=/home/workstation/go/src/github.com/hyperledger" >> /etc/profile
-sudo echo "export FABRIC=/home/workstation/go/src/github.com/hyperledger/fabric" >> /etc/profile
-source /etc/profile
-chown root /etc/profile
-cd $HYPERLEDGER
-git clone https://github.com/hyperledger/fabric.git
+mv go /usr/lib/go1.13.1
+echo "export GOROOT=/usr/lib/go1.13.1" >> /etc/profile
+export GOROOT=/usr/lib/go1.13.1
+cp -r /usr/lib/go1.13.1/bin/*  /usr/bin
+
+mkdir -p  go/src/github.com/hyperledger
+cd go/src/github.com/hyperledger
 git clone https://github.com/hyperledger/fabric-ca.git
-git clone https://github.com/hyperledger/fabric-samples
-cp -r *  $WORKSTATION
-cd /home/workstation/Downloads
-rm -rf *
+export GOPATH=/tmp/go
+git clone https://github.com/hyperledger/fabric.git
+git clone https://github.com/hyperledger/fabric-cli.git
+
+cd fabric
 
 
-cd $FABRIC/scripts
-./bootstrap.sh
+git checkout release-1.4
+make release
+echo " ********************************"
+echo " Verify fabric release  build then continue "
+read ans3  -p
+
+cd ../fabric-ca
+git checkout release-1.4
+make fabric-ca-server
+echo " *********************************"
+echo " Verify fabric-ca-server "
+read ans4 -p
+make fabric-ca-client
+echo " *********************************"
+echo " Verify fabric-ca-client build "
+read ans5 -p
+cd ../fabric-cli
+make
+echo " *********************************"
+echo " Verify fabric-cli build          "
+
+read ans6 -p
+
+mkdir -p /usr/lib/hyperledger/fabric/release-1.4
+mkdir -p /usr/lib/hyperledger/fabric-ca/release-1.4
+mkdir -p /usr/lib/hyperledger/fabric-cli
+
+cp -r /tmp/go/src/github.com/hyperledger/fabric/release/linux-amd64/  /usr/lib/hyperledger/fabric/release-1.4
+cp -r /tmp/go/src/github.com/hyperledger/fabric-ca/bin  /usr/lib/hyperledger/fabric-ca/release-1.4
+cp -r /tmp/go/src/github.com/hyperledger/fabric-cli/bin  /usr/lib/hyperledger/fabric-cli
+
+ln -s /usr/lib/hyperledger/fabric/release-1.4/bin /usr/lib/hyperledger/fabric/bin
+ln -s /usr/lib/hyperledger/fabric-ca/release-1.4/bin /usr/lib/hyperledger/fabric-ca/bin
+
+chmod +x /usr/lib/hyperledger/fabric/bin/
+chmod +x /usr/lib/hyperledger/fabric-ca/bin/
+chmod +x /usr/lib/hyperledger/fabric-cli/bin/
+
+echo "## HYPERLEDGER PATHS"
+echo "export PATH=/usr/lib/hyperledger/fabric/bin:${PATH}" >> /etc/profile
+echo "export PATH=/usr/lib/hyperledger/fabric-ca/bin:${PATH}" >> /etc/profile
+echo "export PATH=/usr/lib/hyperledger/fabric-cli/bin:${PATH}" >> /etc/profile
+echo "export FABRIC_CA_SERVER_HOME=/home/hyperledger/fabric-ca/server" >> /etc/profile
+echo "export FABRIC_CA_CLIENT_HOME=/home/hyperledger/fabric-ca/client" >> /etc/profile
+echo "export FABRIC_CFG_PATH=/home/hyperledger/config" >> /etc/profile
+source /etc/profile
+mkdir -p $FABRIC_CA_SERVER_HOME
+chown -R workstation:workstation /home/hyperledger/
+mkdir -p $FABRIC_CA_CLIENT_HOME
+mkdir -p $FABRIC_CFG_PATH
+
+usermod -aG sudo workstation
+usermod -aG docker workstation
 
 
